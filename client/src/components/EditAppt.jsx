c786a1b16a512fd85e8dcd33751eb42788cf7258
VlnX96 <vrowan69@gmail.com>
2018-11-29 11:35:29 -0600

    in progress

--- client/src/components/EditAppt.jsx
+++ client/src/components/EditAppt.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { appendFile } from 'fs';
+// import { appendFile } from 'fs';
 
 class EditAppt extends Component {
 
@@ -45,7 +45,8 @@ class EditAppt extends Component {
 
     handleFirst(e) {
         // console.log(e.target.value);
-        this.setState({ firstname: e.target.value });
+        this.setState({ 
+            firstname: e.target.value });
     }
 
     handleLast(e) {
@@ -93,6 +94,8 @@ class EditAppt extends Component {
                 body: JSON.stringify(this.state)
             });
             this.props.history.replace(`/api/appointments/${this.props.match.params.id}`);
+            console.log(email, lastname, age, number, other);
+
         } catch (err) { }
     }
 
@@ -122,84 +125,82 @@ class EditAppt extends Component {
                     id={this.state.id}
                 />
 
-            <form>
-                <div>
+                <form action="/appointments" method="GET" style={{ display: 'flex', justifyItems: 'center' }}>
+                    <div>
 
-                    <div className="form-row">
-                        <div className="form-group col-md-4">
-                            <label for="person"></label>
-                            <input type="text" className="form-control" aria-describedby="textHelp" placeholder="First Name" />
+                        <div className="form-row">
+                            <div className="form-group col-md-5">
+                                <label for="person"></label>
+                                <input type="text" className="form-control" aria-describedby="textHelp" placeholder="First Name"
+                                    onChange={e => this.setState({ firstname: e.target.value })} />
+                            </div>
+                            <div className="form-group col-md-5">
+                                <label for="person"></label>
+                                <input type="text" className="form-control" aria-describedby="textHelp" placeholder="Last Name" onChange={e => this.setState({ lastname: e.target.value })} />
+                            </div>
+                            <div className="form-group col-md-2">
+                                <label for="age"></label>
+                                <input type="number" className="form-control" aria-describedby="textHelp" maxLength="2" placeholder="Age" onChange={e => this.setState({ age: e.target.value })} />
+                            </div>
                         </div>
-                        <div className="form-group col-md-4">
-                            <label for="person"></label>
-                            <input type="text" className="form-control" aria-describedby="textHelp" placeholder="Last Name" />
+
+
+                        <div className="form-row">
+                            <div className="form-group col-md-5">
+                                <label for="email"></label>
+                                <input type="email" className="form-control" aria-describedby="textHelp" placeholder="Email" onChange={e => this.setState({ email: e.target.value })} />
+                            </div>
+                            <div className="form-group col-md-4">
+                                <label for="exampleInputPassword1"></label>
+                                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" onChange={e => this.setState({ password: e.target.value })} />
+                            </div>
+                            <div className="form-group col-md-3">
+                                <label for="number"></label>
+                                <input type="number" className="form-control" aria-describedby="textHelp" maxLength="10" placeholder="Phone Number" onChange={e => this.setState({ number: e.target.value })} />
+                            </div>
                         </div>
-                        <div className="form-group col-md-2">
-                            <label for="age"></label>
-                            <input type="number" className="form-control" aria-describedby="textHelp" placeholder="Age" />
+
+
+                        <div className="form-row">
+                            <div className="col-md-3">
+                                <select class="custom-select custom-select-md mb-3">
+                                    <option selected>Gender</option>
+                                    <option value="male">Male</option>
+                                    <option value="female">Female</option>
+                                </select>
+                            </div>
+
+                            <div className="col-md-5">
+                                <select class="custom-select" size="2">
+                                    <option selected>Insurance</option>
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                    <option onChange={e => this.setState({ other: e.target.value })} value="other">Other</option>
+                                </select>
+                            </div>
+
+                            <div className="form-group col-md-4" style={{ display: 'flex', alignItems: 'flex-start' }}>
+                                <label for="other"></label>
+                                <input type="text" className="form-control" aria-describedby="textHelp" placeholder="(if other)" onChange={e => this.setState({ other: e.target.value })} />
+                            </div>
+
                         </div>
+
+                        <div className="form-row">
+                            <div className="form-group col-md-10">
+                                <label for="symptom">What can we help you with?</label>
+                                <input type="text" className="form-control" aria-describedby="textHelp" placeholder="Description" onChange={e => this.setState({ description: e.target.value })} />
+                            </div>
+
+                            <div id="bookIt" className="col-md-2" style={{ display: 'flex', alignItems: 'center', marginTop: '2.5%' }}>
+                                <button type="submit" className="btn btn-primary">Schedule</button>
+                            </div>
+                        </div>
+
                     </div>
-
-
-                    <div className="form-row">
-                        <div className="form-group col-md-4">
-                            <label for="email"></label>
-                            <input type="email" className="form-control" aria-describedby="textHelp" placeholder="Email" />
-                        </div>
-                        <div className="form-group col-md-4">
-                            <label for="exampleInputPassword1"></label>
-                            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
-                        </div>
-                        <div className="form-group col-md-3">
-                            <label for="number"></label>
-                            <input type="number" className="form-control" aria-describedby="textHelp" placeholder="Phone Number" />
-                        </div>
-                    </div>
-
-
-                    <div className="form-row">
-                        <div className="col-md-2">
-                            <select class="custom-select custom-select-md mb-3">
-                                <option selected>Gender</option>
-                                <option value="male">Male</option>
-                                <option value="female">Female</option>
-                            </select>
-                        </div>
-
-                        <div className="col-md-5">
-                            <select class="custom-select" size="2">
-                                <option selected>Insurance</option>
-                                <option value="1">One</option>
-                                <option value="2">Two</option>
-                                <option value="3">Three</option>
-                                <option value="other">Other</option>
-                            </select>
-                        </div>
-
-                        <div className="form-group col-md-4" style={{ display: 'flex', alignItems: 'flex-start' }}>
-                            <label for="other"></label>
-                            <input type="text" className="form-control" aria-describedby="textHelp" placeholder="(if other)" />
-                        </div>
-
-                    </div>
-
-
-
-
-                    <div className="form-row">
-                        <div className="form-group col-md-9">
-                            <label for="symptom">What can we help you with?</label>
-                            <input type="text" className="form-control" aria-describedby="textHelp" placeholder="Description" />
-                        </div>
-
-                        <div id="bookIt" className="col-md-3" style={{ display: 'flex', alignItems: 'center', marginTop: '1.8%' }}>
-                            <button type="submit" className="btn btn-primary">Schedule</button>
-                        </div>
-                    </div>
-
-                </div>
-            </form>
-        </React.Fragment>
+                </form>
+            </React.Fragment>
         )
     }
 }
--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import HelloWorld from './hello';
 import GoodbyeWorld from './goodbye';
 import Appointments from './Appointments';
+import EditAppt from './EditAppt';
+import ApptDetails from './ApptDetails'
+
 
 class Navigation extends Component {
 
@@ -14,6 +17,8 @@ class Navigation extends Component {
                     <Switch>
                         <Route exact path="/" component={GoodbyeWorld} />
                         <Route exact path="/appointments" component={Appointments} />
+                        <Route exact path="appointments/:id" component={ApptDetails} />
+                        <Route exact path="/appointments/:id/edit" component={EditAppt} />
                     </Switch>
                 </Fragment>
             </Router>
